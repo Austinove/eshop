8905c415de669dd278c1a7ad688d6a55f317d9f6
Austinove <bryanovicaustenove@gmail.com>
2023-03-31 20:31:01 +0300

    installed cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 const mysql = require("mysql");
+const cors = require("cors");
 
 require("dotenv/config");
 
@@ -11,39 +12,7 @@ const api = process.env.API_URL;
 app.use(express.json());
 app.use(morgan("tiny"));
 
-app.get(`${api}/products`, (req, res) => {
-  const products = {
-    id: 1,
-    name: "haire dresser",
-    image: "some_url",
-  };
-  res.send(products);
-});
-
-app.post(`${api}/products`, (req, res) => {
-  const newProduct = req.body;
-  res.send(newProduct);
-});
-
 require("./routes/routes.js")(app);
-
-// const con = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "nodedb",
-//   port: 3309,
-// });
-
-// con.connect((err) => {
-//   if (err) throw err;
-//   var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
-//   con.query(sql, (err, result) => {
-//     if (err) throw err;
-//     console.log("table created");
-//   });
-//   console.log("connected");
-// });
 
 app.listen(3000, () => {
   console.log("server is running now");
